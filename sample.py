98b523aa7ebe717c04a639b5d80026a7fe260769
Peatech <90410753+Peatech@users.noreply.github.com>
2024-07-03 15:47:16 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -22,11 +22,10 @@ def sample(N, b, e, m, sigma, eps, save_dir, log_dir):
     logits = mnist.mnist_fully_connected_model(data_placeholder, hidden1, hidden2)
     loss = mnist.loss(logits, labels_placeholder)
     eval_correct = mnist.evaluation(logits, labels_placeholder)
-    train_op = mnist.training(loss, learning_rate=0.001)
 
     # Assuming you have a function to run the differentially private federated averaging:
     Accuracy_accountant, Delta_accountant = run_differentially_private_federated_averaging(
-        loss, train_op, eval_correct, DATA, data_placeholder, labels_placeholder,
+        loss, None, eval_correct, DATA, data_placeholder, labels_placeholder,
         b=batch_size, e=e, m=m, sigma=sigma, eps=eps,
         save_dir=save_dir, log_dir=log_dir
     )
@@ -41,7 +40,7 @@ if __name__ == '__main__':
     parser.add_argument('--sigma', type=float, default=0, help='The gm variance parameter; will not affect if Priv_agent is set to True')
     parser.add_argument('--eps', type=float, default=8, help='Epsilon')
     parser.add_argument('--m', type=int, default=0, help='Number of clients participating in a round')
-    parser.add_argument('--b', type=int, default=10, help='Batches per client')
+    parser.add_argument('--b', type=int, default 10, help='Batches per client')
     parser.add_argument('--e', type=int, default=4, help='Epochs per client')
     parser.add_argument('--log_dir', type=str, default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'), 'tensorflow/mnist/logs/fully_connected_feed'), help='Directory to put the log data.')
     FLAGS = parser.parse_args()
