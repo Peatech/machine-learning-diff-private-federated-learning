14164bed141a11ee29b1a72e34643ccf01314c0b
Peatech <90410753+Peatech@users.noreply.github.com>
2024-07-03 16:34:59 +0100

    Update mnist_inference.py

--- mnist_inference.py
+++ mnist_inference.py
@@ -1,47 +1,61 @@
+import numpy as np
+import math
 import tensorflow as tf
 
 NUM_CLASSES = 10
 IMAGE_SIZE = 28
 IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
 
+def init_weights(size):
+    return np.float32(truncnorm.rvs(-2, 2, size=size) * 1.0 / math.sqrt(float(size[0])))
+
+def inference(images, Hidden1, Hidden2):
+    with tf.name_scope('hidden1'):
+        weights = tf.Variable(init_weights([IMAGE_PIXELS, Hidden1]), name='weights', dtype=tf.float32)
+        biases = tf.Variable(np.zeros([Hidden1]), name='biases', dtype=tf.float32)
+        hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
+
+    with tf.name_scope('hidden2'):
+        weights = tf.Variable(init_weights([Hidden1, Hidden2]), name='weights', dtype=tf.float32)
+        biases = tf.Variable(np.zeros([Hidden2]), name='biases', dtype=tf.float32)
+        hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
+
+    with tf.name_scope('out'):
+        weights = tf.Variable(init_weights([Hidden2, NUM_CLASSES]), name='weights', dtype=tf.float32)
+        biases = tf.Variable(np.zeros([NUM_CLASSES]), name='biases', dtype=tf.float32)
+        logits = tf.matmul(hidden2, weights) + biases
+
+    return logits
+
+def inference_no_bias(images, Hidden1, Hidden2):
+    with tf.name_scope('hidden1'):
+        weights = tf.Variable(init_weights([IMAGE_PIXELS, Hidden1]), name='weights', dtype=tf.float32)
+        hidden1 = tf.nn.relu(tf.matmul(images, weights))
+
+    with tf.name_scope('hidden2'):
+        weights = tf.Variable(init_weights([Hidden1, Hidden2]), name='weights', dtype=tf.float32)
+        hidden2 = tf.nn.relu(tf.matmul(hidden1, weights))
+
+    with tf.name_scope('out'):
+        weights = tf.Variable(init_weights([Hidden2, NUM_CLASSES]), name='weights', dtype=tf.float32)
+        logits = tf.matmul(hidden2, weights)
+
+    return logits
+
+class LossLayer(tf.keras.layers.Layer):
+    def call(self, logits, labels):
+        labels = tf.cast(labels, tf.int64)
+        return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+
+class EvaluationLayer(tf.keras.layers.Layer):
+    def call(self, logits, labels):
+        correct = tf.equal(tf.argmax(logits, axis=1), tf.cast(labels, tf.int64))
+        return tf.reduce_sum(tf.cast(correct, tf.int32))
+
 def placeholder_inputs(batch_size):
-    """Generate placeholder variables to represent the input tensors.
-    Args:
-    batch_size: The batch size will be baked into both placeholders.
-    Returns:
-    images_placeholder: Images placeholder.
-    labels_placeholder: Labels placeholder.
-    """
     images_placeholder = tf.keras.Input(shape=(IMAGE_PIXELS,), batch_size=batch_size, name='images_placeholder')
     labels_placeholder = tf.keras.Input(shape=(), batch_size=batch_size, dtype=tf.int32, name='labels_placeholder')
     return images_placeholder, labels_placeholder
 
-def mnist_fully_connected_model(data_placeholder, hidden1_units, hidden2_units):
-    """Builds the MNIST model up to where it may be used for inference.
-    Args:
-    data_placeholder: Images placeholder.
-    hidden1_units: Size of the first hidden layer.
-    hidden2_units: Size of the second hidden layer.
-    Returns:
-    logits: Output tensor with the computed logits.
-    """
-    hidden1 = tf.keras.layers.Dense(hidden1_units, activation='relu')(data_placeholder)
-    hidden2 = tf.keras.layers.Dense(hidden2_units, activation='relu')(hidden1)
-    logits = tf.keras.layers.Dense(NUM_CLASSES)(hidden2)
-    return logits
-
-def loss(logits, labels):
-    """Calculates the loss from the logits and the labels."""
-    labels = tf.cast(labels, tf.int64)
-    return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
-
-def training(loss, learning_rate):
-    """Sets up the training Ops."""
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-    train_op = optimizer.minimize(loss)
-    return train_op
-
-def evaluation(logits, labels):
-    """Evaluate the quality of the logits at predicting the label."""
-    correct = tf.equal(tf.argmax(logits, axis=1), tf.cast(labels, tf.int64))
-    return tf.reduce_sum(tf.cast(correct, tf.int32))
+def mnist_fully_connected_model(images_placeholder, hidden1_units, hidden2_units):
+    return inference_no_bias(images_placeholder, hidden1_units, hidden2_units)
