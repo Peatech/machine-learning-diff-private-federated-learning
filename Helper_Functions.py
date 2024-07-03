a3cdb8e90967e269ff9f9ecd2039da783871dc6d
Peatech <90410753+Peatech@users.noreply.github.com>
2024-07-03 15:54:06 +0100

    Update Helper_Functions.py

--- Helper_Functions.py
+++ Helper_Functions.py
@@ -15,25 +15,25 @@ class PrivAgent:
         self.N = N
         self.Name = Name
         if N == 100:
-            self.m = [30] * 100
-            self.Sigma = [1] * 24
+            self.m = [30]*100
+            self.sigma = [1]*24
             self.bound = 0.001
         if N == 1000:
-            self.m = [100] * 10
-            self.Sigma = [1] * 24
+            self.m = [100]*10
+            self.sigma = [1]*24
             self.bound = 0.00001
         if N == 10000:
-            self.m = [300] * 10
-            self.Sigma = [1] * 24
+            self.m = [300]*10
+            self.sigma = [1]*24
             self.bound = 0.000001
-        if N != 100 and N != 1000 and N != 10000:
+        if(N != 100 and N != 1000 and N != 10000 ):
             print('!!!!!!! YOU CAN ONLY USE THE PRIVACY AGENT FOR N = 100, 1000 or 10000 !!!!!!!')
 
     def get_m(self, r):
         return self.m[r]
 
-    def get_Sigma(self, r):
-        return self.Sigma[r]
+    def get_sigma(self, r):
+        return self.sigma[r]
 
     def get_bound(self):
         return self.bound
@@ -41,17 +41,22 @@ class PrivAgent:
 def Assignements(dic):
     return [tf.assign(var, dic[Vname_to_Pname(var)]) for var in tf.trainable_variables()]
 
+
 def Vname_to_Pname(var):
     return var.name[:var.name.find(':')] + '_placeholder'
+
 
 def Vname_to_FeedPname(var):
     return var.name[:var.name.find(':')] + '_placeholder:0'
 
+
 def Vname_to_Vname(var):
     return var.name[:var.name.find(':')]
 
+
 class WeightsAccountant:
-    def __init__(self, sess, model, Sigma, real_round):
+    def __init__(self,sess,model,sigma, real_round):
+
         self.Weights = [np.expand_dims(sess.run(v), -1) for v in tf.trainable_variables()]
         self.keys = [Vname_to_FeedPname(v) for v in tf.trainable_variables()]
 
@@ -61,7 +66,7 @@ class WeightsAccountant:
         # Create a list out of the model dictionary in the order in which the graph holds them:
 
         self.global_model = [model[k] for k in self.keys]
-        self.Sigma = Sigma
+        self.sigma = sigma
         self.Updates = []
         self.median = []
         self.Norms = []
@@ -70,12 +75,13 @@ class WeightsAccountant:
         self.num_weights = len(self.Weights)
         self.round = real_round
 
-    def save_params(self, save_dir):
-        filehandler = open(save_dir + '/Wweights_accountant_round_' + str(self.round) + '.pkl', "wb")
+    def save_params(self,save_dir):
+        filehandler = open(save_dir + '/Wweights_accountant_round_'+str(self.round) + '.pkl', "wb")
         pickle.dump(self, filehandler)
         filehandler.close()
 
     def allocate(self, sess):
+
         self.Weights = [np.concatenate((self.Weights[i], np.expand_dims(sess.run(tf.trainable_variables()[i]), -1)), -1)
                         for i in range(self.num_weights)]
 
@@ -85,49 +91,55 @@ class WeightsAccountant:
         # matrices are stacked along.
 
     def compute_updates(self):
+
         # To compute the updates, we subtract the global model from each individual weight matrix. Note:
         # self.Weights[i] is of size [q x p x m], where m is the number of clients whose matrices are stored.
         # global_model['i'] is of size [q x p], in order to broadcast correctly, we have to add a dim.
-        self.Updates = [self.Weights[i] - np.expand_dims(self.global_model[i], -1) for i in range(self.num_weights)]
+
+        self.Updates = [self.Weights[i]-np.expand_dims(self.global_model[i], -1) for i in range(self.num_weights)]
         self.Weights = None
 
     def compute_norms(self):
+
         # The norms List shall have 6 entries, each of size [1x1xm], we keep the first two dimensions because
         # we will later broadcast the Norms onto the Updates of size [q x p x m]
+
         self.Norms = [np.sqrt(np.sum(
-            np.square(self.Updates[i]), axis=tuple(range(self.Updates[i].ndim)[:-1]), keepdims=True)) for i in range(self.num_weights)]
+            np.square(self.Updates[i]), axis=tuple(range(self.Updates[i].ndim)[:-1]),keepdims=True)) for i in range(self.num_weights)]
 
     def clip_updates(self):
         self.compute_updates()
         self.compute_norms()
 
         # The median is a list of 6 entries, each of size [1x1x1],
+
         self.median = [np.median(self.Norms[i], axis=-1, keepdims=True) for i in range(self.num_weights)]
 
         # The factor is a list of 6 entries, each of size [1x1xm]
-        factor = [self.Norms[i] / self.median[i] for i in range(self.num_weights)]
+
+        factor = [self.Norms[i]/self.median[i] for i in range(self.num_weights)]
         for i in range(self.num_weights):
             factor[i][factor[i] > 1.0] = 1.0
 
-        self.ClippedUpdates = [self.Updates[i] / factor[i] for i in range(self.num_weights)]
+        self.ClippedUpdates = [self.Updates[i]/factor[i] for i in range(self.num_weights)]
 
     def Update_via_GaussianMechanism(self, sess, Acc, FLAGS, Computed_deltas):
         self.clip_updates()
         self.m = float(self.ClippedUpdates[0].shape[-1])
         MeanClippedUpdates = [np.mean(self.ClippedUpdates[i], -1) for i in range(self.num_weights)]
 
-        GaussianNoise = [(1.0 / self.m * np.random.normal(loc=0.0, scale=float(self.Sigma * self.median[i]), size=MeanClippedUpdates[i].shape)) for i in range(self.num_weights)]
+        GaussianNoise = [(1.0/self.m * np.random.normal(loc=0.0, scale=float(self.sigma * self.median[i]), size=MeanClippedUpdates[i].shape)) for i in range(self.num_weights)]
 
-        Sanitized_Updates = [MeanClippedUpdates[i] + GaussianNoise[i] for i in range(self.num_weights)]
+        Sanitized_Updates = [MeanClippedUpdates[i]+GaussianNoise[i] for i in range(self.num_weights)]
 
-        New_weights = [self.global_model[i] + Sanitized_Updates[i] for i in range(self.num_weights)]
+        New_weights = [self.global_model[i]+Sanitized_Updates[i] for i in range(self.num_weights)]
 
         New_model = dict(zip(self.keys, New_weights))
 
-        t = Acc.accumulate_privacy_spending(0, self.Sigma, self.m)
+        t = Acc.accumulate_privacy_spending(0, self.sigma, self.m)
         delta = 1
-        if FLAGS.record_privacy:
-            if not FLAGS.relearn:
+        if FLAGS.record_privacy == True:
+            if FLAGS.relearn == False:
                 # I.e. we never learned a complete model before and have therefore never computed all deltas.
                 for j in range(len(self.keys)):
                     sess.run(t)
@@ -143,17 +155,17 @@ def create_save_dir(FLAGS):
     :return: Returns a path that is used to store training progress; the path also identifies the chosen setup uniquely.
     '''
     raw_directory = FLAGS.save_dir + '/'
-    if FLAGS.gm:
-        gm_str = 'Dp/'
-    else:
-        gm_str = 'non_Dp/'
+    if FLAGS.gm: gm_str = 'Dp/'
+    else: gm_str = 'non_Dp/'
     if FLAGS.priv_agent:
-        model = gm_str + 'N_' + str(FLAGS.n) + '/Epochs_' + str(int(FLAGS.e)) + '_Batches_' + str(int(FLAGS.b))
+        model = gm_str + 'N_' + str(FLAGS.n) + '/Epochs_' + str(
+            int(FLAGS.e)) + '_Batches_' + str(int(FLAGS.b))
         return raw_directory + str(model) + '/' + FLAGS.PrivAgentName
     else:
-        model = gm_str + 'N_' + str(FLAGS.n) + '/sigma_' + str(FLAGS.Sigma) + '_C_' + str(FLAGS.m) + '/Epochs_' + str(
+        model = gm_str + 'N_' + str(FLAGS.n) + '/Sigma_' + str(FLAGS.sigma) + '_C_'+str(FLAGS.m)+'/Epochs_' + str(
             int(FLAGS.e)) + '_Batches_' + str(int(FLAGS.b))
         return raw_directory + str(model)
+
 
 def load_from_directory_or_initialize(directory, FLAGS):
     '''
@@ -164,6 +176,7 @@ def load_from_directory_or_initialize(directory, FLAGS):
     :param PrivacyAgent:
     :return:
     '''
+
     Accuracy_accountant = []
     Delta_accountant = [0]
     model = []
@@ -179,6 +192,7 @@ def load_from_directory_or_initialize(directory, FLAGS):
             os.makedirs(directory)
         print('No loadable model found. All updates stored at: ' + directory)
         print('... Initializing a new model ...')
+
 
     else:
         # If there is a model, we have to check whether:
@@ -237,6 +251,7 @@ def load_from_directory_or_initialize(directory, FLAGS):
             raise KeyboardInterrupt
     return model, Accuracy_accountant, Delta_accountant, Acc, real_round, FLAGS, Computed_Deltas
 
+
 def save_progress(save_dir, model, Delta_accountant, Accuracy_accountant, PrivacyAgent, FLAGS):
     '''
     This function saves our progress either in an existing file structure or writes a new file.
@@ -252,29 +267,29 @@ def save_progress(save_dir, model, Delta_accountant, Accuracy_accountant, Privac
     pickle.dump(model, filehandler)
     filehandler.close()
 
-    if not FLAGS.relearn:
+    if FLAGS.relearn == False:
         # I.e. we know that there was no progress stored at 'save_dir' and we create a new csv-file that
         # Will hold the accuracy, the deltas, the m's and we also save the model learned as a .pkl file
 
         with open(save_dir + '/specs.csv', 'wb') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            if FLAGS.priv_agent:
-                writer.writerow([0] + [PrivacyAgent.get_m(r) for r in range(len(Delta_accountant) - 1)])
-            if not FLAGS.priv_agent:
-                writer.writerow([0] + [FLAGS.m] * (len(Delta_accountant) - 1))
+            if FLAGS.priv_agent == True:
+                writer.writerow([0]+[PrivacyAgent.get_m(r) for r in range(len(Delta_accountant)-1)])
+            if FLAGS.priv_agent == False:
+                writer.writerow([0]+[FLAGS.m]*(len(Delta_accountant)-1))
             writer.writerow(Delta_accountant)
             writer.writerow(Accuracy_accountant)
 
-    if FLAGS.relearn:
+    if FLAGS.relearn == True:
         # If there already is progress associated to the learned model, we do not need to store the deltas and m's as
         # they were already saved; we just keep track of the accuracy and append it to the already existing .csv file.
         # This will help us later on to average the performance, as the variance is very high.
 
-        if len(Accuracy_accountant) > 1 or len(Accuracy_accountant) == 1 and FLAGS.loaded:
+        if len(Accuracy_accountant) > 1 or len(Accuracy_accountant) == 1 and FLAGS.loaded is True:
             # If we already appended a new line to the .csv file, we have to delete that line.
             with open(save_dir + '/specs.csv', 'r+w') as csvfile:
                 csvReader = csv.reader(csvfile, delimiter=",")
-                lines = []
+                lines =[]
                 for row in csvReader:
                     lines.append([float(i) for i in row])
                 lines = lines[:-1]
@@ -289,6 +304,7 @@ def save_progress(save_dir, model, Delta_accountant, Accuracy_accountant, Privac
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(Accuracy_accountant)
 
+
 def global_step_creator():
     global_step = [v for v in tf.global_variables() if v.name == "global_step:0"][0]
     global_step_placeholder = tf.placeholder(dtype=tf.float32, shape=(), name='global_step_placeholder')
@@ -298,8 +314,10 @@ def global_step_creator():
     set_global_step = tf.assign(global_step, global_step_placeholder)
     return increase_global_step, set_global_step
 
+
 def bring_Accountant_up_to_date(Acc, sess, rounds, PrivAgent, FLAGS):
     '''
+
     :param Acc: A Privacy accountant
     :param sess: A tensorflow session
     :param rounds: the number of rounds that the privacy accountant shall iterate
@@ -311,23 +329,24 @@ def bring_Accountant_up_to_date(Acc, sess, rounds, PrivAgent, FLAGS):
 
     for r in range(rounds):
         if FLAGS.priv_agent:
-            Sigma = PrivAgent.get_Sigma(r)
+            sigma = PrivAgent.get_sigma(r)
             m = PrivAgent.get_m(r)
         else:
-            Sigma = FLAGS.sigma
+            sigma = FLAGS.sigma
             m = FLAGS.m
-        print('Completed ' + str(r + 1) + ' out of ' + str(rounds) + ' rounds')
-        t = Acc.accumulate_privacy_spending(0, Sigma, m)
+        print('Completed '+str(r+1)+' out of '+str(rounds)+' rounds')
+        t = Acc.accumulate_privacy_spending(0, sigma, m)
         sess.run(t)
         sess.run(t)
         sess.run(t)
     print('The accountant is up to date!')
 
-def print_loss_and_accuracy(global_loss, accuracy):
+def print_loss_and_accuracy(global_loss,accuracy):
     print(' - Current Model has a loss of:           %s' % global_loss)
     print(' - The Accuracy on the validation set is: %s' % accuracy)
     print('--------------------------------------------------------------------------------------')
     print('--------------------------------------------------------------------------------------')
+
 
 def print_new_comm_round(real_round):
     print('--------------------------------------------------------------------------------------')
@@ -338,11 +357,11 @@ def check_validaity_of_FLAGS(FLAGS):
     FLAGS.priv_agent = True
     if not FLAGS.m == 0:
         if FLAGS.sigma == 0:
-            print('\n \n -------- If m is specified the Privacy Agent is not used, then Sigma has to be specified too. --------\n \n')
+            print('\n \n -------- If m is specified the Privacy Agent is not used, then sigma has to be specified too. --------\n \n')
             raise NotImplementedError
     if not FLAGS.sigma == 0:
-        if FLAGS.m == 0:
-            print('\n \n-------- If Sigma is specified the Privacy Agent is not used, then m has to be specified too. -------- \n \n')
+        if FLAGS.m ==0:
+            print('\n \n-------- If sigma is specified the Privacy Agent is not used, then m has to be specified too. -------- \n \n')
             raise NotImplementedError
     if not FLAGS.sigma == 0 and not FLAGS.m == 0:
         FLAGS.priv_agent = False
