5c2bd9ab94baae06318e9a870ebe239ca9b14ec6
Peatech <90410753+Peatech@users.noreply.github.com>
2024-07-03 15:02:56 +0100

    Update mnist_inference.py

--- mnist_inference.py
+++ mnist_inference.py
@@ -26,44 +26,50 @@ apply gradients.
 This file is used by the various "fully_connected_*.py" files and not meant to
 be run.
 """
-
 import tensorflow as tf
 import numpy as np
+from scipy.stats import truncnorm
+import math
 
 NUM_CLASSES = 10
 IMAGE_SIZE = 28
-IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
+IMAGE_PIXELS = IMAGE_SIZE * IMAGE_PIXELS
 
 def init_weights(shape, stddev=0.1):
     """ Initialize weights with a truncated normal distribution """
-    initializer = tf.initializers.TruncatedNormal(stddev=stddev)
-    return tf.Variable(initializer(shape=shape), dtype=tf.float32)
+    values = truncnorm(-2, 2, scale=stddev).rvs(np.prod(shape)).reshape(shape)
+    return tf.Variable(values, dtype=tf.float32)
 
 def mnist_fully_connected_model(hidden1, hidden2):
     """Builds a fully connected model with two hidden layers using tf.keras"""
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(hidden1, activation='relu', input_shape=(IMAGE_PIXELS,), 
-                              kernel_initializer=tf.initializers.TruncatedNormal(stddev=1.0 / np.sqrt(IMAGE_PIXELS))),
-        tf.keras.layers.Dense(hidden2, activation='relu', 
-                              kernel_initializer=tf.initializers.TruncatedNormal(stddev=1.0 / np.sqrt(hidden1))),
-        tf.keras.layers.Dense(NUM_CLASSES, 
-                              kernel_initializer=tf.initializers.TruncatedNormal(stddev=1.0 / np.sqrt(hidden2)))
+        tf.keras.layers.Input(shape=(IMAGE_PIXELS,)),  # Define input shape directly in the Input layer
+        tf.keras.layers.Dense(hidden1, activation='relu', kernel_initializer=tf.initializers.TruncatedNormal(stddev=1.0 / np.sqrt(IMAGE_PIXELS))),
+        tf.keras.layers.Dense(hidden2, activation='relu', kernel_initializer=tf.initializers.TruncatedNormal(stddev=1.0 / np.sqrt(hidden1))),
+        tf.keras.layers.Dense(NUM_CLASSES)
     ])
     return model
-
-def compile_model(model, learning_rate=0.001):
-    """Compiles the model with cross-entropy loss and a gradient descent optimizer"""
-    model.compile(
-        optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=['accuracy']
-    )
 
 def placeholder_inputs(batch_size):
     """Generate placeholder variables to represent the input tensors."""
     images_placeholder = tf.keras.Input(shape=(IMAGE_PIXELS,), batch_size=batch_size, name='images_placeholder')
     labels_placeholder = tf.keras.Input(shape=(), batch_size=batch_size, dtype=tf.int32, name='labels_placeholder')
     return images_placeholder, labels_placeholder
+
+def loss(logits, labels):
+    """Calculates the loss from the logits and the labels."""
+    return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+
+def training(loss, learning_rate=0.001):
+    """Sets up the training operations."""
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    train_op = optimizer.minimize(loss)
+    return train_op
+
+def evaluation(logits, labels):
+    """Evaluate the quality of the logits at predicting the label."""
+    correct = tf.equal(tf.argmax(logits, 1), labels)
+    return tf.reduce_sum(tf.cast(correct, tf.int32))
 
 def mnist_cnn_model():
     """Builds a convolutional neural network model using tf.keras"""
@@ -80,22 +86,6 @@ def mnist_cnn_model():
     ])
     return model
 
-def loss(logits, labels):
-    """Calculates the loss from the logits and the labels."""
-    return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
-
-def training(model, loss, learning_rate):
-    """Sets up the training operations."""
-    optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
-    train_op = optimizer.minimize(loss, var_list=model.trainable_variables)
-    return train_op
-
-def evaluation(logits, labels):
-    """Evaluate the quality of the logits at predicting the label."""
-    correct = tf.equal(tf.argmax(logits, 1), labels)
-    return tf.reduce_sum(tf.cast(correct, tf.int32))
-
-# The following functions are placeholders and should be updated if additional functionalities are needed
 def train_model(model, train_dataset, epochs=10):
     """Function to train the model"""
     model.fit(train_dataset, epochs=epochs)
@@ -103,3 +93,4 @@ def train_model(model, train_dataset, epochs=10):
 def evaluate_model(model, test_dataset):
     """Function to evaluate the model"""
     return model.evaluate(test_dataset)
+
