2aadf4ddc09a508161b704b222e0ce10474dab3f
Peatech <90410753+Peatech@users.noreply.github.com>
2024-07-03 11:22:51 +0100

    Update Create_clients.py

--- Create_clients.py
+++ Create_clients.py
@@ -4,54 +4,45 @@ import os
 
 def create_clients(num, dir):
 
-    '''
-    This function creates clients that hold non-iid MNIST data according to the experiments in https://research.google.com/pubs/pub44822.html. (it actually just creates indices that point to data.
-    but the way these indices are grouped, they create a non-iid client.)
-    :param num: Number of clients
-    :param dir: where to store
-    :return: _
-    '''
-
     num_examples = 50000
     num_classes = 10
 
-    if os.path.exists(dir + '/'+str(num)+'_clients.pkl'):
-        print('Client exists at: '+dir + '/'+str(num)+'_clients.pkl')
+    # Check if clients file already exists
+    if os.path.exists(os.path.join(dir, f'{num}_clients.pkl')):
+        print(f'Client exists at: {os.path.join(dir, f"{num}_clients.pkl")}')
         return
 
+    # Ensure directory exists
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-    # Load the data from .npy files
+    # Load data from .npy files
     x_train = np.load(os.path.join(dir, 'x_train.npy'))
     y_train = np.load(os.path.join(dir, 'y_train.npy'))
 
     buckets = []
     for k in range(num_classes):
         temp = []
-        for j in range(int(num // 100)):  # Ensure integer division
-            temp = np.hstack((temp, k * num_examples // 10 + np.random.permutation(int(num_examples // 10))))
+        for j in range(int(num / 100)):
+            temp = np.hstack((temp, k * num_examples/10 + np.random.permutation(int(num_examples/10))))
         buckets = np.hstack((buckets, temp))
-
+    
     shards = 2 * num
     perm = np.random.permutation(shards)
-    # z will be of length 250 and each element represents a client.
     z = []
     ind_list = np.split(buckets, shards)
     for j in range(0, shards, 2):
-        # each entry of z is associated to two shards. the two shards are sampled randomly by using the permutation matrix
-        # perm and stacking two shards together using vstack. Each client now holds 250*2 datapoints.
         z.append(np.hstack((ind_list[int(perm[j])], ind_list[int(perm[j + 1])])))
-        # shuffle the data in each element of z, so that each client doesn't have all digits stuck together.
         perm_2 = np.random.permutation(int(2 * len(buckets) / shards))
         z[-1] = z[-1][perm_2]
 
-    filehandler = open(dir + '/'+str(num)+'_clients.pkl', "wb")
-    pickle.dump(z, filehandler)
-    filehandler.close()
-    print('client created at: '+dir + '/'+str(num)+'_clients.pkl')
+    # Save clients file
+    with open(os.path.join(dir, f'{num}_clients.pkl'), 'wb') as filehandler:
+        pickle.dump(z, filehandler)
+    
+    print(f'Client created at: {os.path.join(dir, f"{num}_clients.pkl")}')
 
 if __name__ == '__main__':
     List_of_clients = [100, 200, 500, 1000, 2000, 5000, 10000]
     for j in List_of_clients:
-        create_clients(j, os.getcwd()+'/DATA/clients')
+        create_clients(j, os.path.join(os.getcwd(), 'DATA', 'clients'))
